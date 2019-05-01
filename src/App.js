cd0bad910929d2949d7a02be89251a9d3730fd8e
Shuler <msmehaf2@gmail.com>
2019-04-30 20:07:25 -0500

    added npm package for cards and deck variable for minimum number of players

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './components/Card'
+import Card from 'react-playing-card'
 
-class App extends Component {
+let deck = [
+          {"S": "C", "R": "2"},
+          {"S": "D", "R": "4"},
+          {"S": "D", "R": "6"},
+          {"S": "D", "R": "8"},
+          {"S": "D", "R": "10"},
+          {"S": "H", "R": "Q"},
+          {"S": "C", "R": "K"},
+          {"S": "S", "R": "A"}
+]
+
+class App extends Component { 
+
+  state = {
+    suit: deck[0].S,
+    rank: deck[0].R,
+  }
+
+
 
 
   shuffle(array) {
@@ -14,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Card></Card>
+      <Card rank={this.state.rank} suit={this.state.suit}/>
       </div>
     );
   }
--- src/components/Card/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Card() {
-  return (
-      <div><p>This is the CARD!!!</p></div>
-  )
-}
-
-export default Card;
